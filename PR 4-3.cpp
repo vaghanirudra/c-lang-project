3dd65fc8acfa80a8f571679910882ad53497513d
vaghanirudra <vaghanirudra75@gmail.com>
2026-04-15 18:32:44 +0530

    Add files via upload

---
+++ PR 4-3.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,j,k;
+	
+	for(i = 1;i <= 5;i++){
+		for(k = 1;k<=5-i;k++)
+		printf(" ");
+		
+	for(j = 6-i;j<=5;j++)
+	   printf("%d ",j);
+	   
+	   printf("\n");
+	   
+	}
+}
+
+/*
+	output
+	  5
+  	 4 5
+    3 4 5
+   2 3 4 5
+  1 2 3 4 5
+
+*/

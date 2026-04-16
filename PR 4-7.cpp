484233770dd1c907af79276f8889889c94fddb8b
vaghanirudra <vaghanirudra75@gmail.com>
2026-04-16 18:03:41 +0530

    Add files via upload

---
+++ PR 4-7.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		if(i==1||i==3)
+		{
+			for(j=1;j<=5;j++)
+			printf(" * ");
+		}
+		else if(i==2)
+		{
+			printf(" * ");
+			for(j=1;j<4;j++)
+			printf("   ");
+			printf("   * ");
+		}
+		else
+		printf(" * ");
+		printf("\n");
+	}
+}
+
+/*
+   output
+    *  *  *  *  *
+ 	*             *
+	*  *  *  *  *
+	*
+	*
+
+*/
+
+
+
+

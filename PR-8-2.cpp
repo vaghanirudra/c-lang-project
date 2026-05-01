f4fcde08e0dbb05d8b2b90bcb4d6d3cf90928716
vaghanirudra <vaghanirudra75@gmail.com>
2026-05-01 16:22:56 +0530

    Update PR-8-2.cpp

--- PR-8-2.cpp
+++ PR-8-2.cpp
@@ -1,8 +1,46 @@
 #include<stdio.h>
-
-int main()
+void Cubes(int *arr,int size) 
 {
-	
+    printf("\nCubes of all elements:\n");
+    for (int i=0;i<size*size;i++) {
+        int val=*(arr+i);
+        int cube=val*val*val;   
+        printf("%d ",cube);
+        if ((i+1)%size==0) 
+		{
+            printf("\n");
+        }
+    }
 }
-	
+int main()
+ {
+    int size;
+    printf("Enter array size: ");
+    scanf("%d", &size);
+    int a[size][size];
+    printf("Enter array elements:\n");
+    for (int i=0;i<size;i++) 
+	{
+        for (int j=0;j<size;j++) 
+		{
+            printf("a[%d][%d]=",i,j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+    Cubes((int *)a,size);
+}
+
+/*
+	output
+	Enter array size: 2
+	Enter array elements:
+	a[0][0]=3
+	a[0][1]=2
+	a[1][0]=5
+	a[1][1]=4
+
+	Cubes of all elements:
+	27 8
+	 64
+*/
    
